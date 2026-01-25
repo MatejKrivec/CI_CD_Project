9d74f93d6298a5028ecd7143f7daeb1106a13631
MatejKrivec <matej.krivec6@gmail.com>
2026-01-25 16:26:39 +0100

    initial commit

---
+++ ci-cd-project/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="container">
+      <h1>My CI/CD Test App</h1>
+      <p>This is a simple page used to test deployment.</p>
+    </div>
+  )
+}
+
+export default App
